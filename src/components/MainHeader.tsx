16e31a07b82f54ace932ffe20b17844e8d4d243a
Christopher Hallett <chrisjameshal@gmail.com>
2021-11-01 14:06:22 +0000

    Search bar input can be accessed inside parent

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import episodes from "/home/2110-014-cha/Developer/academy/training/tv-show-react/src/episodes.json";
 
+interface SearchBarProps {
+  searchText: string;
+  handleSearchText: (searchInput: string) => void;
+}
+
 function MainHeader(): JSX.Element {
+  const [text, setText] = useState("");
+
   return (
     <header>
       <h1>Game of Thrones</h1>
-      <h2>Episode List</h2>
-      <SearchBar />
+      <h2>Episode List</h2> 
+      <SearchBar searchText={text} handleSearchText={setText}/>
+      <p>{text}</p>
     </header>
   );
 }
 
-function SearchBar(): JSX.Element {
+function SearchBar(props: SearchBarProps): JSX.Element {
   return (
     <>
-      <input type="text" placeholder="Search"></input>
+      <input 
+        type="text" 
+        placeholder="Search" 
+        value={props.searchText} 
+        onChange={(e) => props.handleSearchText(e.target.value)}
+      />
       <p>
         Showing {episodes.length} out of {episodes.length} episodes.
       </p>
